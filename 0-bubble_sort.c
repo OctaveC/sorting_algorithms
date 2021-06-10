0c8c8d6b6588257072e09f85847ecb99c5b739ee
OctaveC <octavec@outlook.com>
2021-06-10 16:12:15 +0000

    added exo 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,22 @@
+#include "sort.h"
+
+void bubble_sort(int *array, size_t size)
+{
+	unsigned int a, swap, ite, ite2;
+
+	for (swap = 0, ite = 0; swap != 1; ite++)
+	{
+		swap = 1;
+		for (ite2 = 0; ite2 < size - 1; ite2++)
+		{
+			if (array[ite2] > array[ite2 + 1])
+			{
+				a = array[ite2];
+				array[ite2] = array[ite2 + 1];
+				array[ite2 + 1] = a;
+				swap = 0;
+				print_array(array, size);
+			}
+		}
+	}
+}
